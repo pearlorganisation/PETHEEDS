0d15f26988897090b57dc3c62dc873998fa3da51
Rahul Rawat <rahulrawat@pearlorganisation.com>
2024-02-29 14:38:46 +0530

    svgfvi

--- src/pages/Components/Product/ProductList.jsx
+++ src/pages/Components/Product/ProductList.jsx
@@ -11,7 +11,7 @@ import img8 from "../../../../Images for Website/Bone and Joint and Motleyvit Co
 import img9 from "../../../../Images for Website/Bone and Joint and Motleyvit Combo/3.png";
 import img10 from "../../../../Images for Website/Bone and Joint and Motleyvit Combo/4.png";
 import img11 from "../../../../Images for Website/Bone and Joint and Motleyvit Combo/5.png";
-
+import { FaCartPlus } from "react-icons/fa";
 const products = [
   {
     id: 1,
@@ -37,7 +37,7 @@ const products = [
   },
   {
     id: 3,
-    name: "Nomad Tumbler",
+    name: "Earthen Bottle",
     quantity: "250ml",
     href: "#",
     price: " ₹350",
@@ -59,7 +59,7 @@ const products = [
   },
   {
     id: 5,
-    name: "Nomad Tumbler",
+    name: "Earthen Bottle",
     quantity: "250ml",
     href: "#",
     price: " ₹350",
@@ -149,8 +149,8 @@ export default function ProductList() {
      
 
 <div class="text-center p-10">
-    <h1 class="font-bold text-4xl mb-4">Explore Our Pawsome Product Collection!</h1>
-    <h1 class="text-3xl">Tailwind CSS</h1>
+    <h1 class="font-bold text-4xl mb-4">Explore Our  Product Collection!</h1>
+    <h1 class="text-3xl"></h1>
 </div>
 
 
@@ -161,7 +161,7 @@ export default function ProductList() {
             return <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
             <a href="#">
                 <img 
-                src={img}
+                src={el.imageSrc}
                         alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
                 <div class="px-4 py-3 w-72">
                     <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
@@ -171,13 +171,18 @@ export default function ProductList() {
                         <del>
                             <p class="text-sm text-gray-600 cursor-auto ml-2">{el.price}</p>
                         </del>
-                        <div class="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                        <div class="ml-auto">
+                            
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                 fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd"
                                     d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
                                 <path
                                     d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                            </svg></div>
+                            </svg>
+                            
+
+                            </div>
                     </div>
                 </div>
             </a>
