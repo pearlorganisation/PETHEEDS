d7545c78c5bb6da8482df9a02557699d2ae09361
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2024-04-04 12:29:14 +0530

    worked on skeleton loader of product cards

--- src/pages/Components/Product/ProductList.jsx
+++ src/pages/Components/Product/ProductList.jsx
@@ -188,94 +188,101 @@ export default function ProductList() {
         </div>
       </div>
 
-      {isLoading ? (
-        <div className="grid place-content-center h-[50vh] w-full">
-          <div class="custom-loader"></div>
-        </div>
-      ) : (
-        <section
-          id="Projects"
-          class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
-        >
-          {productsData?.map((el, id) => {
-            return (
-              <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-                <Link to={`/singleproduct/${el._id}`}>
-                  <img
-                    src={el?.productImg?.path}
-                    alt="Product"
-                    class="h-80 w-72 object-cover rounded-t-xl"
-                  />
 
-                  <div class="px-4 py-3 w-72">
-                    <span class="text-gray-400 mr-3 uppercase text-xs">
-                      Brand
-                    </span>
+      <section
+        id="Projects"
+        class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
+      >
+        {isLoading ? Array(8).fill(true).map(item => {
+          return <div class="w-80 bg-gray-300 animate-pulse shadow-md rounded-md duration-500 p-3 space-y-3">
+            <div class="bg-gray-200 w-full h-[18rem] rounded-md"></div>
+            <div class="bg-gray-200 h-4 w-20"></div>
+            <div class="bg-gray-200 h-4 w-32"></div>
+            <div class="bg-gray-200 h-4 w-14"></div>
+            <div class="bg-gray-200 h-4 w-32"></div>
+            <div class="bg-gray-200 w-full h-9 rounded-md"></div>
 
-                    <p class="text-lg font-bold text-black truncate block capitalize">
-                      {el?.productName}
+
+          </div>
+        }) : productsData?.map((el, id) => {
+          return (
+            <div class="w-80 bg-white  shadow-md rounded-md p-3 space-y-3">
+              <Link to={`/singleproduct/${el._id}`}>
+                <img
+                  src={el?.productImg?.path}
+                  alt="Product"
+                  class="h-80 w-full object-cover rounded-t-xl"
+                />
+
+                <div class="px-4 py-3">
+                  <span class="text-gray-400 mr-3 uppercase text-xs">
+                    Brand
+                  </span>
+
+                  <p class="text-lg font-bold text-black truncate block capitalize">
+                    {el?.productName}
+                  </p>
+
+                  <div class="flex flex-col">
+                    <p class="text-lg font-semibold text-black cursor-auto mt-3 ">
+                      ₹{el?.price}
                     </p>
 
-                    <div class="flex flex-col">
-                      <p class="text-lg font-semibold text-black cursor-auto mt-3 ">
-                      ₹{el?.price}
-                      </p>
-
-                      <div className="flex items-center">
-                        <del className="text-slate-600 mt-3 mb-3 mr-3">
-                          <p class="text-sm text-slate-500 cursor-auto ml-2 ">
+                    <div className="flex items-center">
+                      <del className="text-slate-600 mt-3 mb-3 mr-3">
+                        <p class="text-sm text-slate-500 cursor-auto ml-2 ">
                           ₹{el.price}
-                          </p>
-                        </del>
-                        <p className="border py-1 px-1 rounded-md  text-xs font-semibold text-white bg-indigo-600">
-                          10% OFF
                         </p>
-                      </div>
+                      </del>
+                      <p className="border py-1 px-1 rounded-md  text-xs font-semibold text-white bg-indigo-600">
+                        10% OFF
+                      </p>
+                    </div>
 
-                      <div className="flex mb-3">
-                        <button className="border border-slate-400 rounded-md text-xs p-1 mr-2 text-slate-600 hover:bg-[#4F46E5] hover:text-white hover:border-none">
-                          450ml
-                        </button>
-                        <button className="border border-slate-400 rounded-md text-xs p-1 text-slate-600 hover:bg-[#4F46E5] hover:text-white hover:border-none">
-                          450ml
-                        </button>
-                      </div>
+                    <div className="flex mb-3">
+                      <button className="border border-slate-400 rounded-md text-xs p-1 mr-2 text-slate-600 hover:bg-[#4F46E5] hover:text-white hover:border-none">
+                        450ml
+                      </button>
+                      <button className="border border-slate-400 rounded-md text-xs p-1 text-slate-600 hover:bg-[#4F46E5] hover:text-white hover:border-none">
+                        450ml
+                      </button>
                     </div>
                   </div>
-                </Link>
-
-                <div className="w-full">
-                  <button
-                    className="w-full rounded-lg border bg-blue-700 hover:bg-blue-800 h-12 flex items-center justify-center px-4 my-2" // Added margin here
-                    onClick={() => {
-                      dispatch(addToCart(el));
-                    }}
-                  >
-                    <div className="mr-1">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        fill="white"
-                        class="bi bi-bag-plus"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                        />
-                        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                      </svg>
-                    </div>
-
-                    <div className="text-white font-semibold">ADD TO CART</div>
-                  </button>
                 </div>
+              </Link>
+
+              <div className="w-full">
+                <button
+                  className="w-full rounded-md border bg-blue-700 text-sm hover:bg-blue-800 h-9 flex items-center justify-center gap-3 px-4 my-2" // Added margin here
+                  onClick={() => {
+                    dispatch(addToCart(el));
+                  }}
+                >
+                  <div className="mr-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="white"
+                      class="bi bi-bag-plus"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                      />
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                  </div>
+
+                  <div className="text-white font-semibold">ADD TO CART</div>
+                </button>
               </div>
-            );
-          })}
-        </section>
-      )}
+            </div>
+          );
+        })}
+      </section>
+
     </>
   );
 }
