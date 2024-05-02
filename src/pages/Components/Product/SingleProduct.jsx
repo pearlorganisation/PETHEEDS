380b1ca816b255571d9633a05a03889df3e07324
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2024-05-02 16:42:00 +0530

    added changes on single product

--- src/pages/Components/Product/SingleProduct.jsx
+++ src/pages/Components/Product/SingleProduct.jsx
@@ -36,6 +36,9 @@ const SingleProduct = () => {
       setProductImage(allImages)
     }
   }, [singleProduct])
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+  }, [])
 
   return (
     <>
