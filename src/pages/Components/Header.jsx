6c753275454a741947b5b92563f6e9679ffca222
Rahul Rawat <rahulrawat@pearlorganisation.com>
2024-05-03 12:50:03 +0530

    rahul lo__

--- src/pages/Components/Header.jsx
+++ src/pages/Components/Header.jsx
@@ -118,19 +118,18 @@ const Header = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-   
   return (
     <>
       <div className="relative w-full bg-white py-5 border">
         <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
           <div className="inline-flex items-center space-x-2">
             <a href="/">
-            <img
-              src="https://petheeds.in/wp-content/uploads/2022/09/Petheeds-Aspect-Logo-1.png"
-              className="w-[120px]"
-            />
+              <img
+                src="https://petheeds.in/wp-content/uploads/2022/09/Petheeds-Aspect-Logo-1.png"
+                className="w-[120px]"
+              />
             </a>
-          
+
             <span className="font-bold"></span>
           </div>
           <div className="hidden grow items-start lg:flex">
@@ -154,9 +153,7 @@ const Header = () => {
               <button
                 type="button"
                 className="rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-               
                 onClick={handelnavigatelogin}
-                 
               >
                 Log In
               </button>
@@ -187,16 +184,17 @@ const Header = () => {
           </div>
           {isMenuOpen && (
             <div className="absolute inset-x-0 top-0 z-50 origin-top-right transform p-2 transition lg:hidden">
+             
               <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
                 <div className="px-5 pb-6 pt-5">
                   <div className="flex items-center justify-between">
                     <div className="inline-flex items-center space-x-2">
-                    <a href="/">
-            <img
-              src="https://petheeds.in/wp-content/uploads/2022/09/Petheeds-Aspect-Logo-1.png"
-              className="w-[120px]"
-            />
-            </a>
+                      <a href="/">
+                        <img
+                          src="https://petheeds.in/wp-content/uploads/2022/09/Petheeds-Aspect-Logo-1.png"
+                          className="w-[120px]"
+                        />
+                      </a>
                     </div>
                     <div className="-mr-2">
                       <button
@@ -218,12 +216,15 @@ const Header = () => {
                           href={item.href}
                           className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50"
                         >
+
                           <span className="ml-3 text-base font-medium text-gray-900">
                             {item.name}
                           </span>
+
                           <span>
                             <ChevronRight className="ml-3 h-4 w-4" />
                           </span>
+                          
                         </a>
                       ))}
                     </nav>
@@ -237,7 +238,7 @@ const Header = () => {
                     >
                       Log In
                     </button>
-                    
+
                     <button
                       type="button"
                       className="w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
